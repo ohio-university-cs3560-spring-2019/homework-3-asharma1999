104637fb38e7b7d2a8c927ef012d4ebdcc925d95
Anirudh Sharma <anisharma34@gmail.com>
2019-02-11 23:18:44 -0500

    IGNORE this file, this is not part of the homwork, I don't know how to delete this out of the directory yet.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <array>
+#include "hw_broken.cc"
 
-int deviation( int* a, int n);
+double deviation(int* &a, int &n);
 
 int main()
 {
-	  int a[4] = {1,2,3,4};
-		std::cout << deviation(a) << std::endl;
+	int n;
+	std::cout << "Enter in the size of the array." << std::endl;
+	std::cin >> n;
+	std::cout << "Enter in the " << n << " numbers that you want in your array." << std::endl;
+	
+	int *a = new int[n];
+	
+	for(int i = 0; i < n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	
+  	std::cout << "The standard deviation is: " << deviation(a, n) << std::endl;
 }
+
