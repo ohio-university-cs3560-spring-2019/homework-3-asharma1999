e055864040ec8b252a5497491376aef6ac6f1fb5
Anirudh Sharma <anisharma34@gmail.com>
2019-02-11 22:13:18 -0500

    This is my latex and html folder for the other 50% of the homework that is on the worksheet.

---
+++ html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deviation',['deviation',['../hw__broken_8cc.html#af457b2ddf37e401a7fb23c9d5ff3f5e6',1,'hw_broken.cc']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['assignment_2d3',['Assignment-3',['../md__r_e_a_d_m_e.html',1,'']]]
+];
