c9b5c6fa3f2d11de3223a6f855973ed08226f1a8
Anirudh Sharma <anisharma34@gmail.com>
2019-02-24 23:54:59 -0500

    These 2 files are for the bash script homework in homework 5 for CS3560.

---
+++ homework5.py
@@ -0,0 +1,14 @@
+import sys
+
+line_count = sys.argv[len(sys.argv) - 4]
+word_count = sys.argv[len(sys.argv) - 3]
+character_count = sys.argv[len(sys.argv) - 2]
+
+print("Number of lines motherfucka!")
+print(line_count)
+
+print("Number of words motherfucka!")
+print(word_count)
+
+print("Number of characters motherfucka!")
+print(character_count)
---
+++ homework5.sh
@@ -0,0 +1,3 @@
+COMMAND_LINE_ARGS = `wc *`
+
+python3 homework5.py COMMAND_LINE_ARGS
