a19c824c8307644b2522fe34ceefc8661512fb43
Anirudh Sharma <anisharma34@gmail.com>
2019-02-25 23:40:55 -0500

    This is the ruby file for homework 6 in CS3560 that takes care of BONUS 1 cases.

--- homework6.rb
+++ homework6.rb
@@ -1,7 +1,11 @@
 
 puts "Enter a string: "
 str = gets
+
 str = str.gsub(/\bf/, "gh")
+str = str.gsub(/\bF/, "gh")
 str = str.gsub(/sh\b/, "ti")
+str = str.gsub(/SH\b/, "ti")
 str = str.gsub(/\Bi\B/, "o")
+str = str.gsub(/\BI\B/, "o")
 puts str
